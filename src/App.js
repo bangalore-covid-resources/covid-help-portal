48081e0bdb8121e265be9adff4327de33771fc6f
PrashanthNagaraj <prashanth.claw@gmail.com>
2021-04-26 16:43:08 +0530

    Use hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import Header from "./components/main/header";
 import Footer from "./components/main/footer";
