3d5132d0f3029b6df3b7cd22ab5222d45616607d
PrashanthNagaraj <prashanth.claw@gmail.com>
2021-04-30 15:21:48 +0530

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import Header from "./components/main/header";
 import Footer from "./components/main/footer";
 import RouterConfig from "./router/routerConfig";
-
-
+import RouteChangeTracker from "./router/routeChangeTracker";
 import "./content/styles/App.scss";
+
+ReactGA.initialize("UA-195969077-1");
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <RouteChangeTracker />;
         <RouterConfig />
         <Footer />
       </div>
